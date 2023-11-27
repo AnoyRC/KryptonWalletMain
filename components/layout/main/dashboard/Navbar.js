235c889cb2774fca01f9c50baf72500ed2ef5837
Gautam Raj <ashragautam25@gmail.com>
2023-11-27 15:15:20 +0530

    Update Submenu styles

--- components/layout/main/dashboard/Navbar.js
+++ components/layout/main/dashboard/Navbar.js
@@ -1,41 +1,40 @@
-"use client";
-import CustomMenu from "./Navbar/CustomMenu";
-import { Button } from "@material-tailwind/react";
+import Link from 'next/link';
+import CustomMenu from './Navbar/CustomMenu';
 
 const UtilityMenu = [
   {
-    title: "Transfer",
-    description: "Transfer your funds to another wallet.",
+    title: 'Transfer',
+    description: 'Transfer your funds to another wallet.',
   },
   {
-    title: "Swap",
-    description: "Swap your tokens for another.",
+    title: 'Swap',
+    description: 'Swap your tokens for another.',
   },
 ];
 
 const AccountMenu = [
   {
-    title: "Transactions",
-    description: "View your transactions.",
+    title: 'Transactions',
+    description: 'View your transactions.',
   },
   {
-    title: "Chats",
-    description: "View your chats.",
+    title: 'Chats',
+    description: 'View your chats.',
   },
   {
-    title: "Requests",
-    description: "View all of your guardian requests and recovery requests",
+    title: 'Requests',
+    description: 'View all of your guardian requests and recovery requests',
   },
 ];
 
 const SettingsMenu = [
   {
-    title: "General",
-    description: "Change your general settings.",
+    title: 'General',
+    description: 'Change your general settings.',
   },
   {
-    title: "2FA",
-    description: "Enable / Change your 2FA settings.",
+    title: '2FA',
+    description: 'Enable / Change your 2FA settings.',
   },
 ];
 
@@ -43,19 +42,35 @@ export default function Navbar() {
   return (
     <div className="w-full flex justify-between font-fhtotal items-center h-[80px]">
       <div className="flex items-center">
-        <h1 className="text-5xl text-black font-fhtotal font-bold mr-5">
+        <p className="text-5xl text-black font-fhtotal font-bold mr-5">
           Krypton
-        </h1>
+        </p>
+
         <div className="flex items-center mt-2 ml-7">
-          <Button
+          <Link
+            href={'/home'}
+            id=":RkrqukqH2:"
             variant="text"
-            className="flex items-center gap-3 text-base mr-4 font-normal capitalize tracking-normal font-uni outline-none"
+            className="align-middle select-none text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none py-3 px-6 rounded-lg text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20 flex items-center gap-3 text-base font-normal capitalize tracking-normal font-uni outline-none"
           >
-            Home{" "}
-          </Button>
-          <CustomMenu title="Utilities" menuItems={UtilityMenu} image="logo1" />
-          <CustomMenu title="Accounts" menuItems={AccountMenu} image="logo2" />
-          <CustomMenu title="Settings" menuItems={SettingsMenu} image="logo3" />
+            Home{' '}
+          </Link>
+
+          <CustomMenu
+            title="Accounts"
+            menuItems={AccountMenu}
+            image="account"
+          />
+          <CustomMenu
+            title="Utilities"
+            menuItems={UtilityMenu}
+            image="utilities"
+          />
+          <CustomMenu
+            title="Settings"
+            menuItems={SettingsMenu}
+            image="setting"
+          />
         </div>
       </div>
     </div>
--- components/layout/main/dashboard/Navbar/CustomMenu.js
+++ components/layout/main/dashboard/Navbar/CustomMenu.js
@@ -1,4 +1,5 @@
-"use client";
+'use client';
+
 import {
   Menu,
   MenuHandler,
@@ -6,11 +7,11 @@ import {
   MenuItem,
   Button,
   Card,
-  Typography,
-} from "@material-tailwind/react";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import Image from "next/image";
+} from '@material-tailwind/react';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function CustomMenu({ title, menuItems, image }) {
   const [openMenu, setOpenMenu] = useState(false);
@@ -21,36 +22,44 @@ export default function CustomMenu({ title, menuItems, image }) {
           variant="text"
           className="flex items-center gap-3 text-base font-normal capitalize tracking-normal font-uni outline-none"
         >
-          {title}{" "}
+          {title}{' '}
           <ChevronDownIcon
             strokeWidth={2.5}
             className={`h-3.5 w-3.5 transition-transform ${
-              openMenu ? "rotate-180" : ""
+              openMenu ? 'rotate-180' : ''
             }`}
           />
         </Button>
       </MenuHandler>
-      <MenuList className="hidden w-[26rem] grid-cols-7 gap-1 overflow-visible lg:grid">
+
+      <MenuList className="hidden w-fit grid-cols-8 gap-4 overflow-visible lg:grid">
         <Card
           shadow={false}
-          className="col-span-3 flex h-full w-full items-center justify-center rounded-2xl p-4 outline-none"
+          className="col-span-4 flex h-full w-full rounded-2xl p-4 outline-none max-w-xs space-y-10 bg-[rgb(255,248,248)]"
         >
-          <div className="w-[80%] h-[80%]">
-            <Image
-              src={"/images/main/dashboard/navbar/" + image + ".svg"}
-              layout="fill"
-              objectFit="contain"
-              className="rounded-2xl"
-            />
+          <div className=" space-y-2">
+            <p className="font-uni text-lg font-bold">Your Account</p>
+            <p className="font-uni text-md">
+              Check what you've spent, chat securely, and handle guardian
+              requests effortlessly.
+            </p>
           </div>
+
+          <Image
+            src={'/images/main/dashboard/navbar/' + image + '.png'}
+            width={200}
+            height={200}
+            className="aspect-square mx-auto"
+          />
         </Card>
-        <ul className="col-span-4 flex w-full flex-col gap-1 outline-none">
+
+        <ul className="col-span-4 flex max-w-xs flex-col gap-1 outline-none">
           {menuItems.map(({ title, description }) => (
             <a href="#" key={title}>
               <MenuItem>
-                <h2 className="font-uni text-lg font-bold">{title}</h2>
+                <p className="font-uni text-lg font-bold">{title}</p>
 
-                <h3 className="font-uni text-md ">{description}</h3>
+                <p className="font-uni text-md">{description}</p>
               </MenuItem>
             </a>
           ))}
