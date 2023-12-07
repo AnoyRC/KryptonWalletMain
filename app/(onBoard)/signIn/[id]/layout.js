940f98d3a3c3b306b5060f46211f3ad68a306be5
Gautam Raj <ashragautam25@gmail.com>
2023-12-07 16:18:36 +0530

    Add sign page

---
+++ app/(onBoard)/signIn/[id]/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Krypton | Sign',
+  description: 'By Bankless DAO',
+};
+
+export default function LoginLayout({ children }) {
+  return <>{children}</>;
+}
--- redux/slice/sigManagerSlice.js
+++ redux/slice/sigManagerSlice.js
@@ -5,22 +5,29 @@ const sigManagerSlice = createSlice({
   initialState: {
     drawer: false,
     signature: '',
+    signDrawer: false,
   },
 
   reducers: {
     openDrawer: (state) => {
       state.drawer = true;
     },
+
     closeDrawer: (state) => {
       state.drawer = false;
     },
+
+    toggleSignDrawer: (state, action) => {
+      state.signDrawer = action.payload;
+    },
+
     setSignature: (state, action) => {
       state.signature = action.payload;
     },
   },
 });
 
-export const { openDrawer, closeDrawer, setSignature } =
+export const { openDrawer, closeDrawer, setSignature, toggleSignDrawer } =
   sigManagerSlice.actions;
 
 export default sigManagerSlice.reducer;
